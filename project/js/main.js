067453830d4aae94f0eab256fdfe874f1ce3812a
eoinfitz14 <eoin.fitzsimons14@gmail.com>
2019-10-06 13:14:01 +0100

    Implemented API using OpenWeatherMap

--- project/js/main.js
+++ project/js/main.js
@@ -1,18 +1,29 @@
-function showPicture(){
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/giphy.gif"/>');
-  $("p").html("High five! You're building your first web app!");
+$(document).ready(function(){
+  getWeather();
+})
 
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
-  
+function getWeather(){
+  // Query parameters (i.e q = ___) are used to specify parts of the request
+  var url = "https://api.openweathermap.org/data/2.5/weather?q=Dublin&units=metric&APPID="+apiKey;
+
+  // ajax was imported in code at start in head.ejs
+  $.ajax(url,{success: function (data){
+    
+    // view logs in javascript console (Chrome->View->Developer->Console)
+    console.log(data);
+    
+    // find object with class of 'city.'
+    // take the data object returned by openWeatherMap, and replace the text of city class with the name 
+    $(".city").text(data.name);
+
+    // similar steps to get temperature from data object into the temp class
+    $(".temp").text(data.main.temp);
+  }})
 }
+
 /*
 Key takeaways:
 - Need to research Node.js and Express.js tutorials to get familiar
 - Need to set up a webapp from scratch (don't forget to 'npm install' when doing so)
 - Helper is used to includes references to css and js files needed for project's pages
-- 
-
-
 */
