0a9b4bf15c09685c1f3a3eeaf9b91ae0eb1fabc3
eoinfitz14 <eoin.fitzsimons14@gmail.com>
2019-10-06 14:21:38 +0100

    firebase set up successfully

--- project/js/main.js
+++ project/js/main.js
@@ -1,6 +1,7 @@
-$(document).ready(function(){
-  getWeather();
-})
+// Can keep this commented as this runs the function immediately
+// $(document).ready(function(){
+//   getWeather(searchQuery);
+// })
 
 function getWeather(searchQuery){ // notice variables are without type
   // Query parameters (i.e q = ___) are used to specify parts of the request
@@ -28,6 +29,27 @@ function getWeather(searchQuery){ // notice variables are without type
 function searchWeather() {
   var searchQuery = $(".search").val();
   getWeather(searchQuery);
+}
+
+function handleSignIn(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user.email);
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
 }
 
 /*
